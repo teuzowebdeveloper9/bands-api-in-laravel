7d6cf0ae04a4e24cf3e03aae958e9e02f5e6cd70
Mateus web developer <mateussoftwaredeveloper@gmail.com>
2025-05-14 18:29:58 -0300

    finish

--- routes/api.php
+++ routes/api.php
@@ -15,17 +15,13 @@ use App\Http\Controllers\BandController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
 Route::get('hello/{nome}', function ($nome){
     return 'hello laravel from your friend '. $nome." bye";
 } );
-
 Route::post('hello-post', 'App\Http\Controllers\HelloLaravelController@hello');
-
 Route::get('bands', 'App\Http\Controllers\BandController@getall');
 Route::get('bands/{id}', 'App\Http\Controllers\BandController@getById');
 Route::get('bands/genre/{genre}','App\Http\Controllers\BandController@getByGenre');
